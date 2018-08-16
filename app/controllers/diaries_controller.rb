c87cbb8ab903b928b8d4a2c1d51bfe12d85c61b3
Bonifase Orwa <orwabonifase@gmail.com>
2018-08-17 00:16:48 +0300

    add create diary action

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,9 +1,38 @@
 class DiariesController < ApplicationController
+    before_action :require_login
+
     def index
-      @diaries = Diary.all
+      @diaries = current_user.diaries
     end
 
     def show
-        @diary = Diary.find(params[:id])
+        @diary = current_user.diaries.find(params[:id])
+    end
+    
+    def new
+        @diary =current_user.diaries.build
+    end
+
+    def create
+        @diary = current_user.diaries.build(diary_params)
+
+        if @diary.save
+            redirect_to diary_path(@diary), notice: "Diary Created!"
+        else
+            @errors = @diary.errors.full_messages
+            render :new
+        end
+    end
+
+    def destroy
+        diary = current_user.diaries.find(params[:id])
+        diary.destroy
+        redirect_to diaries_path, notice: "Deleted Diary: #{diary.title}"
+    end
+
+    private
+
+    def diary_params
+        params.require(:diary).permit(:title, :description)
     end
 end
